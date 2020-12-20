495d0ec49c540cda6ec67dc48e30abd09ee715d0
Kai Klefenz <kklefenz@gmx.de>
2020-12-20 16:52:17 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Adventcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of 2020!");
+
+            int counter = 0;  
+            string line;  
+            List<string> passports = new List<string>();
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"..\adventcode\input\day4.txt");  
+            while((line = file.ReadLine()) != null)  
+            {  
+                // System.Console.WriteLine(line);  
+                counter++;  
+                passports.Add(line);
+            }  
+            
+            file.Close();  
+
+
+            // Suspend the screen.  
+            // System.Console.ReadLine();  
+        }
+    }
+}
